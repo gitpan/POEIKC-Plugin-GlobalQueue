e796b9c754b926fa8ec37043a6d2037b92b6ebbe
Yuji Suzuki <yujisuzuki@mail.arbolbell.jp>
2009-10-17 22:58:09 +0000

    Import of SUZUKI/POEIKC-Plugin-GlobalQueue-0.04 from CPAN.
    gitpan-cpan-distribution: POEIKC-Plugin-GlobalQueue
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         SUZUKI/POEIKC-Plugin-GlobalQueue-0.04.tar.gz
    gitpan-cpan-author:       SUZUKI
    gitpan-cpan-maturity:     released

--- lib/POEIKC/Plugin/GlobalQueue.pm
+++ lib/POEIKC/Plugin/GlobalQueue.pm
@@ -2,7 +2,7 @@ package POEIKC::Plugin::GlobalQueue;
 
 use strict;
 use 5.008_001;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Data::Dumper;
 use Class::Inspector;
@@ -29,7 +29,7 @@ sub spawn
 	my $session = POE::Session->create(
 	    object_states => [ $self =>  Class::Inspector->methods(__PACKAGE__) ]
 	);
-	return $session->ID;
+	return $self;
 }
 
 
